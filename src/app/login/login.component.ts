85808fbacdc600d3e31fc0d96a416efce609ed3b
招文桃 <gdouzwt@gmail.com>
2019-04-14 00:06:14 +0800

    add authentication service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {HardcodedAuthenticationService} from "../service/hardcoded-authentication.service";
 
 @Component({
   selector: 'app-login',
@@ -18,17 +19,21 @@ export class LoginComponent implements OnInit {
   //Dependency Injection
 
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {
+  }
 
   ngOnInit() {
   }
 
   handleLogin() {
     console.log(this.username);
-    if (this.username === "tao" && this.password === "wing") {
+    // if (this.username === "tao" && this.password === "wing") {
     //Redirect to Welcome Page
-    this.router.navigate(['welcome', this.username]);
-    this.invalidLogin =false;
+    if (this.hardcodedAuthenticationService.authenticate(this.username, this.password)) {
+      this.router.navigate(['welcome', this.username]);
+      this.invalidLogin = false;
     } else {
       this.invalidLogin = true;
     }
