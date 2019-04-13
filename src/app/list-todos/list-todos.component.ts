8c52047b8003f2054a05b57a436adccbea2c8919
zhaowentao <gdouzwt@hotmail.com>
2019-04-13 23:31:35 +0800

    menu & navbar

---
+++ src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Todo {
+
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent implements OnInit {
+
+
+  todos = [
+    new Todo(1, 'Learn to Love', false, new Date()),
+    new Todo(2, 'Become an Expert at Angular', false, new Date()),
+    new Todo(3, 'To be with you', false, new Date()),
+  ];
+
+  // todo = {
+  //   id: 1,
+  //   description: 'Learn to Love'
+  // };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
